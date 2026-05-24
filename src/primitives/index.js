e1b0af90ca0f9e219bf475b672e4a30d2a830143
Arka Saha <arkacomputer2004@gmail.com>
2026-05-25 00:50:43 +0530

    Update primitives exports

--- src/primitives/index.js
+++ src/primitives/index.js
@@ -1,3 +1,28 @@
 export * from "./theme.js";
-export { Sidebar as ShadcnSidebar, Button as ShadcnButton, Table as ShadcnTable } from "./shadcn";
-export { Sidebar as AntdSidebar, Button as AntdButton, Table as AntdTable } from "./antd";
+export {
+  Sidebar as ShadcnSidebar,
+  Button as ShadcnButton,
+  Table as ShadcnTable,
+  ProfileDialog as ShadcnProfileDialog,
+  StickyFooterDialog as ShadcnStickyFooterDialog,
+  DialogCloseButton as ShadcnDialogCloseButton,
+  DialogNoCloseButton as ShadcnDialogNoCloseButton,
+  DropDown as ShadcnDropDown,
+  TabsLine as ShadcnTabsLine,
+  TabsVertical as ShadcnTabsVertical,
+  TabsDisabled as ShadcnTabsDisabled,
+} from "./shadcn";
+export {
+  Sidebar as AntdSidebar,
+  Button as AntdButton,
+  Table as AntdTable,
+  ProfileDialog as AntdProfileDialog,
+  StickyFooterDialog as AntdStickyFooterDialog,
+  DialogCloseButton as AntdDialogCloseButton,
+  DialogNoCloseButton as AntdDialogNoCloseButton,
+  DropDown as AntdDropDown,
+  TabsLine as AntdTabsLine,
+  TabsVertical as AntdTabsVertical,
+  TabsDisabled as AntdTabsDisabled,
+} from "./antd";
+
