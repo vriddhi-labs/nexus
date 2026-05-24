7cfa1b735905765254ee948ed60debe4e6e81552
Arka Saha <arkacomputer2004@gmail.com>
2026-05-25 00:48:57 +0530

    Update Ant Design exports index.js

--- src/primitives/antd/index.js
+++ src/primitives/antd/index.js
@@ -1,3 +1,8 @@
 export { Sidebar } from "./Sidebar";
 export { Button } from "./Button";
 export { Table } from "./Table";
+export * from "./Modal";
+export * from "./Dropdown";
+export * from "./Tabs";
+
+
